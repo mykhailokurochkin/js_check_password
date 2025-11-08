2f6782282ed9aae1e295190bc413e28c46e9c845
Mykhailo <kurochkinmy@gmail.com>
2025-11-08 19:15:56 +0100

    test: add tests

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,18 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('1234567890')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('Password1!')).toBe(true);
   });
 
-  // write more tests here
+  it('returns false for too-short, lowercase-only password', () => {
+    expect(checkPassword('qwerty')).toBe(false);
+  });
+
+  it(`should return false for short mixed-case password with symbol`, () => {
+    expect(checkPassword('Str@ng')).toBe(false);
+  });
 });
